bba7172d354b59f786934b6b388ceaa3a5d1baae
João Victor Antonucci <jvictor.antonucci@gmail.com>
2021-08-30 00:35:21 -0300

    🎉 Begin Íllia Movie App

--- illiamovieapp/lib/app/app_widget.dart
+++ illiamovieapp/lib/app/app_widget.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:illiamovieapp/app/app_controller.dart';
 
+import 'core/presentation/widgets/connectivity_banner.dart';
 import 'core/utils/screen/design_ui.dart';
 import 'core/utils/screen/screen_util.dart';
 
@@ -9,7 +12,7 @@ class AppWidget extends StatefulWidget {
   _AppWidgetState createState() => _AppWidgetState();
 }
 
-class _AppWidgetState extends State<AppWidget> {
+class _AppWidgetState extends ModularState<AppWidget, AppController> {
   @override
   Widget build(BuildContext context) {
     return ScreenUtil.builder(
@@ -22,6 +25,34 @@ class _AppWidgetState extends State<AppWidget> {
             primarySwatch: Colors.blue,
           ),
           initialRoute: '/',
+          builder: (context, child) {
+            return Observer(
+              builder: (_) {
+                return Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(
+                        top: controller.isConnected ? 0 : kToolbarHeight / 2.5,
+                      ),
+                      child: child!,
+                    ),
+                    Positioned(
+                      top: 0,
+                      left: 0,
+                      child: Observer(
+                        builder: (_) {
+                          return ConnectivityBanner(
+                            isConnected: controller.isConnected,
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                );
+              },
+            );
+          },
         ).modular();
       },
     );
