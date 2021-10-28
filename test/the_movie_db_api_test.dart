44f340820d96772ba75625fcf8331a8c495b95a0
Rodrigo Nairnei <ronairnei@gmail.com>
2021-10-28 20:31:49 -0300

    feat: increment unit-test api. close #5, close #6, wip #4

--- test/the_movie_db_api_test.dart
+++ test/the_movie_db_api_test.dart
@@ -1,16 +1,18 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-///
 void main() {
   var apiKey = "7e49e84a68cdbea10b67f65709d1bc64";
+
   var baseUrl = "https://api.themoviedb.org";
+  var backDropUrl = "https://image.tmdb.org";
+
   var apiVersion = "3";
   var page = 1;
   var language = "pt-BR";
 
   ///list tendency movie with paginate and language example
-  test("listTendencyMovie()", () async {
+  test("listTendencyMovie", () async {
     var query = "trending/all/day";
     var response = await Dio().get(
         "$baseUrl/$apiVersion/$query?api_key=$apiKey&page=$page&language=$language");
@@ -18,7 +20,7 @@ void main() {
   });
 
   ///list movie on Theaters with paginate and language example
-  test("listCurrentTheatersMovie()", () async {
+  test("listCurrentTheatersMovie", () async {
     var query = "movie/now_playing";
     var response = await Dio().get(
         '$baseUrl/$apiVersion/$query?api_key=$apiKey&page=$page&language=$language');
@@ -26,10 +28,36 @@ void main() {
   });
 
   ///list movie upcoming on Theaters with paginate and language example
-  test("listCurrentTheatersMovie()", () async {
+  test("listCurrentTheatersMovie", () async {
     var query = "movie/upcoming";
     var response = await Dio().get(
         '$baseUrl/$apiVersion/$query?api_key=$apiKey&page=$page&language=$language');
+    expect(response.statusCode, 200);
+  });
+
+  ///get moview details by id
+  test("getMovieDetailsByID", () async {
+    var query = "movie/5";
+    var response =
+        await Dio().get('$baseUrl/$apiVersion/$query?api_key=$apiKey');
+    expect(response.statusCode, 200);
+  });
+
+  ///This have any configuration, i go use a standard configuration
+  ///get image cover movie
+  test("getCover", () async {
+    var query = "/t/p/w500";
+    var backDropPath = "ujWOI68vGkfLOgEDmcrjIS6il7B.jpg";
+    var response = await Dio().get('$backDropUrl/$query/$backDropPath');
+    expect(response.statusCode, 200);
+  });
+
+  ///This have any configuration, i go use a standard configuration
+  ///get the image Poster movie
+  test("getPoster", () async {
+    var query = "/t/p/w500";
+    var posterPath = "ujWOI68vGkfLOgEDmcrjIS6il7B.jpg";
+    var response = await Dio().get('$backDropUrl/$query/$posterPath');
     expect(response.statusCode, 200);
   });
 }
